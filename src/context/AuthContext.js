fab4a25b763c6feaef6b59a32e14c2ad2aa5bbaa
Kalid Mubarek <Kalidmubarek@gmail.com>
2025-04-06 11:27:32 +0300

    Adding Map component  and the Protected route

---
+++ src/context/AuthContext.js
@@ -0,0 +1,32 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import { auth } from '../firebase/config';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    loading
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
